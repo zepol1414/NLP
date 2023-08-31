ad333d2c0ed9ca74cb10224363fa73946c2ea414
Alejandro López <zepol1414@gmail.com>
2023-08-31 18:10:31 +0200

    Embedding and requirements.txt

---
+++ scripts/embedding.py
@@ -0,0 +1,40 @@
+"""
+scripts/embedding.py
+
+Function to embed the text column of the dataset.
+"""
+
+################################################################################
+# IMPORT MODULES
+
+# Import libraries
+import sys
+import spacy
+
+sys.dont_write_bytecode = True  # Disable cache
+
+################################################################################
+# SETTINGS
+################################################################################
+# Load the spacy model
+nlp = spacy.load("en_core_web_lg")
+
+################################################################################
+# MAIN
+################################################################################
+# Define main function
+def function_embedding(df):
+    """
+    Embed the text column of the dataset.
+
+    Args:
+        df (pd.DataFrame): Dataset with two columns, text and category.
+
+    Returns:
+        pd.DataFrame.
+    """
+
+    # Create sentence and label lists
+    sentences = df.text.values
+    text_emb = [nlp(sentence) for sentence in sentences]
+    
